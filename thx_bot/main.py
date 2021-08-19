81ff3deb4fa2b2d5ba3d160b3fe84899de56ad5c
Andrew Kulikov <jadecoresky@gmail.com>
2021-08-19 13:44:39 +0300

    Playing with telegram API

--- thx_bot/main.py
+++ thx_bot/main.py
@@ -1,2 +1,30 @@
+import os
+
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("APUA!!!")
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    updater = Updater(os.getenv("BOT_TOKEN"))
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    updater.start_polling()
+    updater.idle()
+
+
 if __name__ == '__main__':
-    print(123)
+    main()
