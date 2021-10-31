59df989b3d1edb2054c2eb7b4a50267cc36b9e46
Peter Polman <peter@peterpolman.nl>
2021-10-31 21:42:27 +0100

    Startup script access

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-pip install --upgrade pip
-pip install -r ./thx_bot/requirements/requirements.txt -r ./thx_bot/requirements/dev-requirements.txt
-python3 main.py
+pip install -r /home/ubuntu/bot-telegram/thx_bot/requirements/requirements.txt -r /home/ubuntu/bot-telegram/thx_bot/requirements/dev-requirements.txt
+python3 /home/ubuntu/bot-telegram/main.py
